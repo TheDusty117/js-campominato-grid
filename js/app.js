124ca77b73c86d52bcdd1f780870d36fa92dd505
TheDusty117 <gabrieledipassio@gmail.com>
2023-02-10 12:08:27 +0100

    NEED TO REFACTOR WITH FUNCTIONS

--- js/app.js
+++ js/app.js
@@ -4,10 +4,15 @@ console.log('CAMPO MINATO')
 
 document.querySelector('.play-button').addEventListener('click',function(){ //on click compare la griglia
   
+  
+  let askLevel = 0
+  
+  while (askLevel < 10 || askLevel > 20 || isNaN(askLevel)) {
+    askLevel = parseInt(prompt('inserisci difficoltà da 10 a 20'))
+  }
 
-  
-  const askLevel = parseInt(prompt('inserisci difficoltà da 10 a 20'))
-  
+  //variabili dei bottoni?
+
   let latoGriglia = askLevel
   let numeroCelle = latoGriglia * latoGriglia
   
@@ -16,24 +21,24 @@ document.querySelector('.play-button').addEventListener('click',function(){ //on
   const grigliaElement = document.querySelector('.griglia') //prendo il primo ed unico div con classe griglia dal DOM
   console.log(grigliaElement)
 
-  grigliaElement.innerHTML = ('') // SCOMMENTA per dare al button play, anche il reset della griglia
+  grigliaElement.innerHTML = ('') // COMANDO CHE RESETTA IL GIOCO 
   
   //generare dentro il DIV con classe 'griglia' 100 celle con il loro stile
   
   for (let i = 0; i < numeroCelle; i++) {
     let num = i + 1 // numero che associa progressivamente ad ogni giro di for per 100 volte
     console.log(i + 1)
-    const divCreateElement = document.createElement('div'); //creo un tag div ad ogni ITERAZIONE(100 iteraz.)
-    divCreateElement.className = 'cella' // al tag div creto, ci associo la classe 'cella' che ha il suo stile in CSS
-    console.log(divCreateElement)//console per controllare se sono stati creati
-    divCreateElement.style.width = `calc(100% / ${latoGriglia})`
-    divCreateElement.innerHTML = num
+    const cellaElement = document.createElement('div'); //creo un tag div ad ogni ITERAZIONE(100 iteraz.)
+    cellaElement.className = 'cella' // al tag div creto, ci associo la classe 'cella' che ha il suo stile in CSS
+    console.log(cellaElement)//console per controllare se sono stati creati
+    cellaElement.style.width = `calc(100% / ${latoGriglia})`
+    cellaElement.innerHTML = num
     
-    grigliaElement.append(divCreateElement)
+    grigliaElement.append(cellaElement)
     
-    divCreateElement.addEventListener('click', function() { // A OGNI GIRO, SICCOME SIAMO DENTRO IL FOR, verra' creato un EVENTLISTENER CLICK ad ogni iterazione insieme ai numeri e ai vari div con la rispettiva classe
+    cellaElement.addEventListener('click', function() { // A OGNI GIRO, SICCOME SIAMO DENTRO IL FOR, verra' creato un EVENTLISTENER CLICK ad ogni iterazione insieme ai numeri e ai vari div con la rispettiva classe
       console.log(`click ${num}`)     //con il backtick uso template literal, per scrivere stringhe, con il dollaro e graffe gli dico che non e' piu una stringa ma una VARIABILE
-      divCreateElement.style.backgroundColor = 'blue'
+      cellaElement.style.backgroundColor = 'cornflowerblue'
       
     })
   }
@@ -41,7 +46,7 @@ document.querySelector('.play-button').addEventListener('click',function(){ //on
   
 })
 
-//BOTTONE DI RESET TOTALE
+//BOTTONE DI RESET TOTALE E CHIUSURA GRIGLIA
 document.querySelector('.reset-button').addEventListener('click',function(){
 
   const grigliaElement = document.querySelector('.griglia') //prendo il primo ed unico div con classe griglia dal DOM
@@ -51,3 +56,11 @@ document.querySelector('.reset-button').addEventListener('click',function(){
 
 })
 
+
+function onClick(){}
+
+function startGame(){}
+
+function resetGame(){}
+
+function generateGrid(){}
